1c675f20af7ae3fb587d5045d8c3c4dfe2116e50
Kit Yan Choi <kchoi@enthought.com>
2016-04-04 15:32:39 +0100

    double quote the file path

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -6,7 +6,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 coverage run -p -m nose.core -v mayavi
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd integratedtests/mayavi
-coverage run run.py
+coverage run ".\run.py"
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage combine
 coverage report
