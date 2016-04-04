ce9f87befc11367c3420e5a5bc4040b5c52b2010
Kit Yan Choi <kchoi@enthought.com>
2016-04-04 14:59:36 +0100

    rcfile for coverage (appveyor)

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -6,7 +6,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 coverage run -p -m nose.core -v mayavi
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd integratedtests/mayavi
-coverage run -p run.py
+coverage run --rcfile=../../.coveragerc -p run.py
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage combine
 coverage report
